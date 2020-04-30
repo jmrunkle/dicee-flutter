a1bf6eeb44d9d84e452e04f21426bc301e233465
Jason <jmrunkle@gmail.com>
2020-04-29 19:18:42 -0700

    Finished version of dicee project.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,9 +16,66 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDice = 1;
+  int rightDice = 1;
+  static var random = Random.secure();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            Expanded(
+              child: FlatButton(
+                child: Image.asset('images/dice$leftDice.png'),
+                onPressed: () => setState(() {
+                  leftDice = rollDice();
+                }),
+              ),
+            ),
+            Expanded(
+              child: FlatButton(
+                child: Image.asset('images/dice$rightDice.png'),
+                onPressed: () => setState(() {
+                  rightDice = rollDice();
+                }),
+              ),
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 20.0,
+        ),
+        RaisedButton(
+          onPressed: () => setState(() {
+            leftDice = rollDice();
+            rightDice = rollDice();
+          }),
+          child: Text('Roll both'),
+        ),
+        SizedBox(
+          height: 20.0,
+        ),
+        Center(
+          child: Text(
+            'Total: ${leftDice + rightDice}',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20.0,
+            ),
+          ),
+        ),
+      ],
+    );
   }
+
+  int rollDice() => random.nextInt(6) + 1;
 }
